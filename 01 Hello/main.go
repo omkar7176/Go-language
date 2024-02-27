d1d5739afb32a96bf6035f40a4ebdf6e8d8f7ab8
omkart <omkartupere7176@gmail.com>
2024-02-28 00:14:12 +0530

    Added basic structure

---
+++ 01 Hello/main.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hello World")
+}
+
+// 1st run the command --> go mod init hello
+// 2nd run the program --> go run main.go
+//   OP: Hello World
